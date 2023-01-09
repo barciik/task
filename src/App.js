79b204aaaeb16feafc1fa1d95932196b0362e741
bart <92023758+barciik@users.noreply.github.com>
2023-01-09 18:36:01 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const URL = 'https://reqres.in/api/products';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState(null);
+	const [searchInput, setSearchInput] = useState('');
+
+	useEffect(() => {
+		axios.get(URL).then((response) => {
+			setData(response.data);
+		});
+	}, []);
+	if (!data) return null;
+  console.log(data)
+
+	const searchHandler = (e) => {
+		e.preventDefault();
+		setSearchInput(e.target.value);
+	};
+
+	if (searchInput.length > 0) {
+		data.data.filter((item) => item.id.include(searchInput))
+	}
+
+	return (
+		<div className='app'>
+			<form>
+				<input
+					value={searchInput}
+					onChange={searchHandler}
+					className='search-bar'
+					type='number'
+					name='search'
+				></input>
+			</form>
+			<ul className='list'>
+				{data.data.map((item) => {
+					return (
+						<li
+							style={{ backgroundColor: `${item.color}` }}
+							className='list-item'
+							key={item.id}
+						>
+							<span>
+								{item.id} {item.name} {item.year}
+							</span>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { store } from './store';
+import { Provider } from 'react-redux';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<RouterProvider router={router}></RouterProvider>
+		</Provider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
